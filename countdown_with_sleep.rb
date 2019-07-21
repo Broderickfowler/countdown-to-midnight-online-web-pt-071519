9cd9665bdbc1c70ca34b0c45e86637c647796d49
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-07-21 22:39:01 +0000

    Automatically backed up by Learn

--- countdown_with_sleep.rb
+++ countdown_with_sleep.rb
@@ -1,10 +1,3 @@
 def countdown_with_sleep(count)
-  def countdown(number)
-  while number >= 0
-    puts  "#{number} SECOND(S)!"
-    number -= 1
-    sleep 5
-  end
-    "HAPPY NEW YEAR!"
-end
-end
+  
+  
