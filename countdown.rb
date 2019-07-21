4ac74a2289a7777fb169fdef388a5b530cf82a2e
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-07-21 21:16:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,13 @@
 #write your code here
-
+number = 12
 def countdown(number)
   while number > 0
     puts  "#{number} SECOND(S)!"
+    number -= 1
 end
 
 
     
-    until number
+    until number == 
   end
 
