3b8b1a5063e61e55e979dbe639de3c2516a086bf
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-07-21 21:08:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,7 @@
 #write your code here
 
 def countdown(number)
-  while number > 0 
-    puts "#{number} SECOND(S)!"
-    
   
-  
-  until number == 0
-    puts "HAPPY NEW YEAR!"
     number -= 1
   end
 
