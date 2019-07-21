09093acbbf1b332d10c4d0608db824bfcbaeda77
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-07-21 23:11:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
-def countdown(n)
-  while n >= 0
-     puts "#{n} SECOND(S)!"
-    n -= 1
-  end
-     "HAPPY NEW YEAR!"
+def countdown(seconds_to_midnight)
+ while seconds_to_midnight > 0
+   puts "#{seconds_to_midnight} SECOND(S)!"
+   seconds_to_midnight -= 1
+end
+seconds_to_midnight.to_s + " left. HAPPY NEW YEAR!"
 end
 
 
