fa29c79e18d9363ec5c0a627f08672890048c28e
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-07-21 23:13:18 +0000

    Automatically backed up by Learn

--- countdown_with_sleep.rb
+++ countdown_with_sleep.rb
@@ -1,26 +1,10 @@
 def countdown_with_sleep(count)
   count = sleep 5
 def countdown(n)
-  while n >= 0
+  while n > 0
      puts "#{n} SECOND(S)!"
     n -= 1
   end
      "HAPPY NEW YEAR!"
 end
 
-
-def countdown(seconds_to_midnight)
- while seconds_to_midnight > 0
-   puts "#{seconds_to_midnight} SECOND(S)!"
-   seconds_to_midnight -= 1
-end
-seconds_to_midnight.to_s + " left. HAPPY NEW YEAR!"
-end
-def countdown_with_sleep(seconds_to_midnight)
- while seconds_to_midnight > 0
-   puts "#{seconds_to_midnight} SECOND(S)!"
-   sleep(1)
-   seconds_to_midnight -= 1
- end
- seconds_to_midnight.to_s + " left. HAPPY NEW YEAR!"
-end'
